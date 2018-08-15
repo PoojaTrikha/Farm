3d4ab5a67e9c4a1be40eb2c1151de325d76616e1
Pooja <pooja.kaul2015@gmail.com>
2018-08-15 14:20:21 +0200

    super and this

--- oop-farm/crop.js
+++ oop-farm/crop.js
@@ -1,22 +1,44 @@
 // crops.js
+
 class Crop {
     constructor(acres) {
-     this.acres = acres
-   }
- }
+        this.acres = acres
+
+    }
+    getYieldInEuros() {
+        return this.getYieldInKg() * this.price
+    }
+
+    getCosts(){
+        return this.acres * this.cost
+
+    }
+}
 
 class Wheat extends Crop {
-    getYieldInKg() {
-      return Math.pow(this.acres * 1.5, 1.3)
+    constructor(acres, price,cost) {
+        super(acres)
+        this.price = 1.5
+        this.cost = 340
     }
-  }
 
-class SugarCane extends Crop{
     getYieldInKg() {
-        return Math.pow(this.acres * 2.6,1.1)
-      }
+        return Math.pow(this.acres * 1.5, 1.3)
+    }
+}
+
+class SugarCane extends Crop {
+    constructor(acres, price,cost) {
+        super(acres)
+        this.price = 2
+        this.cost = 166
+    }
+
+    getYieldInKg() {
+        return Math.pow(this.acres * 2.6, 1.1)
+    }
 
 }
 
 
- module.exports = { Wheat, Crop, SugarCane }
+module.exports = { Wheat, Crop, SugarCane }
--- oop-farm/crops.spec.js
+++ oop-farm/crops.spec.js
@@ -12,6 +12,12 @@ test('The Wheat class calculates the proper yield', () => {
 })
 
 
+test('Wheat class calculates the proper yield value', () => {
+    const wheat = new Wheat(100)
+    expect(wheat.getYieldInEuros()).toBeCloseTo(1011.6, 1)
+  })
+
+
 test('The SugarCane class can calculate a yield', () => {
     const sugarcane = new SugarCane(100)
     expect(sugarcane.getYieldInKg()).toBeDefined()
@@ -21,8 +27,29 @@ test('The SugarCane class can calculate a yield', () => {
     const sugarcane = new SugarCane(100)
     expect(sugarcane.getYieldInKg()).toBeCloseTo(453.4, 0)
   })
+  
+  test('Sugarcane class calculates the proper yield value', () => {
+    const sugarcane = new SugarCane(100)
+    expect(sugarcane.getYieldInEuros()).toBeCloseTo(906.7, 0)
+  })
 
   test('A crop can store its area size', () => {
     const crop = new Crop(12345)
     expect(crop.acres).toBe(12345)
+  })
+
+
+  test('Sugarcane class calculates the proper costs value', () => {
+    const sugarcane = new SugarCane(0)
+    expect(sugarcane.getCosts()).toBe(0)
+  })
+  
+  test('Sugarcane class calculates the proper costs value', () => {
+    const sugarcane = new SugarCane(456)
+    expect(sugarcane.getCosts()).toBe(75696)
+  })
+  
+  test('Wheat class calculates the proper costs value', () => {
+    const wheat = new Wheat(123)
+    expect(wheat.getCosts()).toBe(41820)
   })
