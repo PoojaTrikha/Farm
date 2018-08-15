ddbdf38eeee95e2433435fa20b297120901a84d5
Pooja <pooja.kaul2015@gmail.com>
2018-08-15 16:19:59 +0200

    Constructor and Super

--- oop-farm/animals.js
+++ oop-farm/animals.js
@@ -1,16 +1,56 @@
 
-class Animal{
+class Animal {
+    constructor(Age) {
+        this.Age = Age
+    }
+
+    getValueInEuros(){
+        return this.getWeightInKg() * this.price
+    }
+
+}
+
+class Pig extends Animal {
+    constructor(Age, price) {
+        super(Age)
+        this.price = 1.5
+        
+    }
+    getWeightInKg() {
     
+        return this.Age * 2.3
+
+    }
+}
+
+class Cow extends Animal {
+    constructor(Age, price) {
+        super(Age)
+        this.price = 4
+        
+    }
+
+    getWeightInKg() {
+
+        return this.Age * 1.5
+    }
+}
+
+class Horse extends Animal {
+    constructor(Age, price) {
+        super(Age)
+        this.price = 10
+        
+    }
+
+    getWeightInKg() {
+     
+        return this.Age * 1.7
+    }
 }
 
 
 
 
 
-
-
-
-
-
-
-module.exports = { Animal }
+module.exports = { Animal, Pig, Horse, Cow}
--- oop-farm/animals.spec.js
+++ oop-farm/animals.spec.js
@@ -1,1 +1,31 @@
-const {Animal } = require('./animals')
+const {Pig, Horse, Cow, Animal} = require('./animals')
+
+test('Pig of 0 days calculates proper weight', () => {
+  const pig = new Pig(0)
+  expect(pig.getWeightInKg(20)).toBe(0)
+})
+
+test('Pig of 10 days calculates proper weight', () => {
+  const pig = new Pig(10)
+  expect(pig.getWeightInKg(10)).toBe(23)
+})
+
+test('Cow of 0 days calculates proper weight', () => {
+  const cow = new Cow(0)
+  expect(cow.getWeightInKg(0)).toBe(0)
+})
+
+test('Cow of 13 days calculates proper weight', () => {
+  const cow = new Cow(13)
+  expect(cow.getWeightInKg(13)).toBe(19.5)
+})
+
+test('Horse of 0 days calculates proper weight', () => {
+  const horse = new Horse(0)
+  expect(horse.getWeightInKg(12)).toBe(0)
+})
+
+test('Horse of 33 days calculates proper weight', () => {
+  const horse = new Horse(33)
+  expect(horse.getWeightInKg(33)).toBe(56.1)
+})
